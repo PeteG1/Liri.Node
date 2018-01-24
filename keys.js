bfaa4335bee003e90728f02f764a3950ec517b5d
Pete <petegarcia54@sbcglobal.net>
2018-01-24 06:07:43 -0600

    reupload

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("Keys loaded");
+
+var twitterKeys = {
+  consumer_key: "	L7PiuWrlc8x672zyxyrUVdWMo",
+  consumer_secret: "CEIQi9byPaEp72iM81nNfKc3M1oWI2vidEqO8omJJtBQfORVf1",
+  access_token_key: "3499611974-b7qNe3mmr0gwgcycPEiAjkdgJF0R2xoqhrBQf5G",
+  access_token_secret: "	SWUySO5bqy7hY9Q9fM7SnJvfUpGdz8jzHPZKemzls0ks7"
+};
+
+module.exports = twitterKeys;
